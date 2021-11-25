de440c2fb214d87779c6e4357703f65eee8eb39a
TomKim <babooteng2@gmail.com>
2021-11-25 16:24:48 +0900

    #2.6~2.7 Super Recap and Themes

--- src/App.js
+++ src/App.js
@@ -1,50 +1,18 @@
-import styled, {keyframes} from "styled-components";
+import styled from "styled-components";
 
 const Wrapper = styled.div`
   display: flex;
-`;
-const rotationAnimation = keyframes`
-  0% {
-    transform:rotate(0deg);
-    border-radius: 0px;
-  }
-  50% {    
-    border-radius: 100px;
-  }
-  100% {
-    transform:rotate(360deg);
-    border-radius: 0px;
-  }
-`;
-const Emoji = styled.span`
-  font-size: 36px;  
-`;
-const Box = styled.div`
-  background-color: tomato;
-  height: 200px;
-  width: 200px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${rotationAnimation} 3s linear infinite;
-  ${Emoji} {    
-    &:hover {
-      font-size: 60px;
-    }
-    &:active {
-      opacity: .5;
-    }
-  }
+  background-color: ${props=>props.theme.backgroundColor}
 `;
 
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>😊</Emoji>
-      </Box>
-      <Emoji>😊</Emoji>
+      <Title>wow</Title>
     </Wrapper>
   )
 }

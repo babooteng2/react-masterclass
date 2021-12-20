e27c1dac5bcb2611b3ddc408821d5d971653c19a
TomKim <babooteng2@gmail.com>
2021-12-20 22:00:45 +0900

    #6.4 Styles and Placeholders

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -4,10 +4,8 @@ import "styled-components";
 // and extend them!
 declare module "styled-components" {
   export interface DefaultTheme {
-    textColor: string;
     bgColor: string;
-    accentColor: string;
-    cardBgColor: string;
-    cardLineColor: string;
+    boardColor: string;
+    cardColor: string;
   }
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,17 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const darkTheme: DefaultTheme = {
-  bgColor: "#2c3e50",
-  textColor: "white",
-  accentColor: "#9b59b6",
-  cardBgColor: "transparent",
-  cardLineColor: "#474787",
-};
-
-export const lightTheme: DefaultTheme = {
-  bgColor: "whitesmoke",
-  textColor: "black",
-  accentColor: "tomato",
-  cardBgColor: "white",
-  cardLineColor: "#d1ccc0",
+  bgColor: "#2f3640",
+  boardColor: "#DADFE9",
+  cardColor: "white",
 };

e8540ac0ffb2cc58ff4cd816f538969afcb6168b
TomKim <babooteng2@gmail.com>
2021-12-23 16:35:42 +0900

    #7.6 Gestures part Two - dragConstraints

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { motion, Variants } from "framer-motion";
 import GlobalStyle from "./GlobalStyle";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -19,22 +20,40 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  border-radius: 40px;
+  background-color: rgba(255, 255, 255, 0.2);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
 const boxVariants: Variants = {
-  hover: { scale: 1.5, rotateZ: 90 },
+  hover: { rotateZ: 90 },
   click: { borderRadius: "100px" },
   drag: { backgroundColor: "rgb(45, 52, 54)" },
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box
-        drag
-        variants={boxVariants}
-        whileHover="hover"
-        whileTap="click"
-        whileDrag="drag"
-      />
+      <GlobalStyle />
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragSnapToOrigin
+          dragElastic={1}
+          dragConstraints={biggerBoxRef}
+          variants={boxVariants}
+          whileHover="hover"
+          whileTap="click"
+          whileDrag="drag"
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }

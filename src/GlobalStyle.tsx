fe1e3747672c1500db0dc8628e40a3ac2f055aad
TomKim <babooteng2@gmail.com>
2021-12-28 22:32:10 +0900

    #8.6 Home Screen part Two - styled component prop and helper func

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -54,9 +54,9 @@ table {
 body {
   font-weight: 300;
   font-family: 'Source Sans Pro', sans-serif;  
-  color:black;
-  line-height: 1.2;
-  background:linear-gradient(135deg,#e09,#d0e);
+  color: ${(props) => props.theme.white.darker};
+  line-height: 1.2;  
+  background-color: black;
 }
 a {
   text-decoration:none;
--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,23 @@
 const BASE_PATH = "https://api.themoviedb.org/3";
 
+interface IMovie {
+  id: number;
+  title: string;
+  overview: string;
+  poster_path: string;
+  backdrop_path: string;
+}
+export interface IGetMoviesResult {
+  dates: {
+    minimun: string;
+    maximun: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
 export function getMovies() {
   return fetch(
     `${BASE_PATH}/movie/now_playing?api_key=${process.env.REACT_APP_TMDB_KEY}`
